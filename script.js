f4decf37cb0aba1f29339e33a5b169f1251ce0f4
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2023-07-31 18:53:02 +0100

    Add game function and modify playRound function
    The game function simulates a game of rock paper scissors spanning five
    rounds.
    
    The playRound function was modified so that it returns a value that can
    determine the winner of a round in a conditional statement.

--- script.js
+++ script.js
@@ -22,27 +22,79 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerChoice === "rock" && computerChoice === "scissors") {
         // rock beats scissors
-        return "You Win! Rock beats Scissors";
+        console.log("You Win! Rock beats Scissors");
+        return "W";
     } else if (playerChoice === "rock" && computerChoice === "paper") {
         // rock loses to paper
-        return "You Lose! Rock loses to Paper";
+        console.log("You Lose! Rock loses to Paper");
+        return "L";
     } else if (playerChoice === "paper" && computerChoice === "rock") {
         // paper beats rock
-        return "You Win! Paper beats Rock";
+        console.log("You Win! Paper beats Rock");
+        return "W";
     } else if (playerChoice === "paper" && computerChoice === "scissors") {
         // paper loses to scissors
-        return "You Lose! Paper loses to Scissors";
+        console.log("You Lose! Paper loses to Scissors");
+        return "L";
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
         // scissors beats paper
-        return "You Win! Scissors beats Paper";
+        console.log("You Win! Scissors beats Paper");
+        return "W";
     } else if (playerChoice === "scissors" && computerChoice === "rock") {
         // scissors loses to rock
-        return "You Lose! Scissors loses to Rock";
+        console.log("You Lose! Scissors loses to Rock");
+        return "L";
     } else if (playerChoice === computerChoice) {
         // same choice ends in a draw
-        return `Draw ${computerSelection} and ${computerSelection} cancel out`;
-    } else {
-        // for invalid inputs
-        return "Your choice must be either Rock, Paper or Scissors";
+        console.log(
+            `Draw ${computerSelection} and ${computerSelection} cancel out`
+        );
+        return "D";
     }
 }
+
+function game() {
+    // assign variable "playerScore" an inital value of 0
+    let playerScore = 0;
+    // assign variable "computerScore" an inital value of 0
+    let computerScore = 0;
+
+    // loop the game 5 times
+    for (let i = 1; i <= 5; i++) {
+        // display the current score of the game
+        console.log(`Player ${playerScore} - ${computerScore} Computer`);
+        // ask the user to choose his weapon
+        let choice = prompt("Choose your weapon. Rock, Paper or Scissors.");
+        // play a round of rock paper scissors
+        let roundResult = playRound(choice, getComputerChoice());
+
+        if (roundResult === "W") {
+            // if the round returns a "W" add a point to player
+            ++playerScore;
+        } else if (roundResult === "L") {
+            // if the round returns a "L" add a point to computer
+            ++computerScore;
+        } else {
+            // if the round returns a "D" don't add a point
+            continue;
+        }
+    }
+
+    // show game over screen
+    console.log("\nGAME OVER!");
+    // enter the final score of the match
+    console.log(`Player ${playerScore} - ${computerScore} Computer`);
+
+    if (playerScore > computerScore) {
+        // if player has more points at the end declare them the winner
+        console.log("Congratulations! You Win!");
+    } else if (playerScore < computerScore) {
+        // if computer has more points at the end declare them the winner
+        console.log("You Lose! Better Luck Next Time.");
+    } else {
+        // if they have the same points declare a draw
+        console.log("It's a Draw! Good Game.");
+    }
+}
+
+game();
